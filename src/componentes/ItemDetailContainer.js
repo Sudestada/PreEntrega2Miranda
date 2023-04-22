181ea6533f8f5875d44afb922dd495ff19bb605b
Cristian Miranda <mr_brettg@yahoo.com.ar>
2023-04-22 13:35:15 -0300

    Finalizar compra agregado

--- src/componentes/ItemDetailContainer.js
+++ src/componentes/ItemDetailContainer.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { getProductsById } from "./asyncMock";
 import { useParams } from "react-router-dom";
+import ItemDetail from "./ItemDetail";
 
 
 
@@ -25,19 +26,14 @@ const ItemDetailContainer = () => {
     if (loading) {
         return <h1>Cargando ...</h1>
     }
-   console.log(typeof product)
+    console.log(product)
 
     return (
 
         <div>
             <h1>Detalle de productos</h1>
             <div>
-                <h2>{product[0].brand}</h2>
-                <img src={product[0].img} alt={product[0].name} />
-                <h3>Precio : $ {product[0].price}</h3>
-                <h3>Descripcion : {product[0].description}</h3>
-                <button>Agregar al carrito</button>
-               
+                <ItemDetail {...product[0]} />
             </div>
 
         </div>
--- src/componentes/itemDetail.js
+++ src/componentes/itemDetail.js
@@ -1,12 +1,41 @@
 import './itemDetail.css';
 import { useContext } from 'react';
-import { Context } from '../componentes/Context/CartContext';
+import { Context } from './Context/CartContext';
+import { useState } from 'react';
+import ItemCount from './ItemCount';
 
-const itemDetail = (id, name, img, price, category, stock) =>{
-    const {addItem} = useContext(Context)
+const ItemDetail = ({ id, brand, img, price, stock }) => {
+    const [quantity, setQuantity] = useState(0)
+    
+    const handleOnAdd = (count) => {
+        const objProductToAdd = { id, brand, price, count }
 
-    const handleOnAdd = (quantity) => {
-        const productToAdd = {id, name, price, quantity}
+        console.log('Agregue al carrito :', count)
+        setQuantity(count)
+ 
     }
-    addItem(productToAdd)
+    return (
+
+        <div>
+            <h2>{brand}</h2>
+            <img src={img} alt={brand} />
+            <h3>Precio : $ {price}</h3>
+            <h3>Stock : {stock}</h3>
+            <footer>
+                {
+                    quantity === 0 ? (
+                        <ItemCount stock={stock} initial={1} onAdd={handleOnAdd}/>
+                    ) : (
+                        <button>Finalizar compra</button>
+                    )
+                }
+            </footer>
+
+
+        </div>
+
+
+    )
 }
+
+export default ItemDetail;
